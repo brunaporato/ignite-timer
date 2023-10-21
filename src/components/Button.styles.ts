eca81b312e30acb32c863589bae255c35e5abfca
Bruna <brunaporato@gmail.com>
2023-10-21 18:51:29 -0300

    first commit

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+
+
+export const ButtonContainer = styled.button`
+  margin: 8px;
+  background: ${props => props.theme["green-300"]};
+  border: 0;
+  padding: 8px;
+`
---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import { ButtonContainer, ButtonVariant } from './Button.styles';
+
+
+export function Button() {
+  return (
+    <ButtonContainer>
+        Enviar
+    </ButtonContainer>
+  )
+}
