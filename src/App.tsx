628742e39d00f31af4199a7e24ed3e4402b886c3
Bruna <brunaporato@gmail.com>
2023-10-22 17:50:03 -0300

    add eslint pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { ThemeProvider } from 'styled-components';
-import { defaultTheme } from './styles/themes/default';
-import { Button } from './components/Button';
-import { GlobalStyle } from './styles/global';
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Button />
       <Button />
-
       <GlobalStyle />
     </ThemeProvider>
   )
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,10 +1,8 @@
-import styled from "styled-components";
-
-
+import styled from 'styled-components'
 
 export const ButtonContainer = styled.button`
   margin: 8px;
-  background: ${props => props.theme["green-300"]};
+  background: ${(props) => props.theme['green-300']};
   border: 0;
   padding: 8px;
 `
