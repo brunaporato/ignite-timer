03a64b8303dcabc1d20c3e5158fb2fca2fe03aad
Bruna <brunaporato@gmail.com>
2023-10-23 22:08:45 -0300

    adjust input at home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,13 +19,21 @@ export function Home() {
             type="text"
             id="task"
             placeholder="Dê um nome para o seu projeto"
+            list="task-sugestions"
           />
+
+          <datalist id="task-sugestions">
+            <option value="Projeto 01" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
